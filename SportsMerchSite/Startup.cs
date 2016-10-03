e2fa210b4dea388f29494b163fcdc1e030550541
shaelarie <shae.larie@gmail.com>
2016-10-03 10:01:50 -0500

    initial upload. blank asp mvc proj

---
+++ SportsMerchSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsMerchSite.Startup))]
+namespace SportsMerchSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
